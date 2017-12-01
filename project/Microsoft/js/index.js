ed8d6e316c9bd725a1e4ad4c2c301e21641c1ba8
yueshangmx <yueshang9310@gmail.com>
2017-12-01 19:45:22 +0800

    2017-12-1

---
+++ project/Microsoft/js/index.js
@@ -0,0 +1,44 @@
+$(".li_1").bind("mousemove",function () {
+  $(".li_1 div")[0].style.backgroundPosition="-168px -112px";
+  $(".li_1 span")[0].style.color="#00aeef";
+})
+$(".li_1").bind("mouseout",function () {
+  $(".li_1 div")[0].style.backgroundPosition="-56px -168px";
+  $(".li_1 span")[0].style.color="#fff";
+})
+
+$(".li_2").bind("mousemove",function () {
+  $(".li_2 div")[0].style.backgroundPosition="-56px 0px";
+  $(".li_2 span")[0].style.color="#00aeef";
+})
+$(".li_2").bind("mouseout",function () {
+  $(".li_2 div")[0].style.backgroundPosition="-56px -112px";
+  $(".li_2 span")[0].style.color="#fff";
+})
+
+$(".li_3").bind("mousemove",function () {
+  $(".li_3 div")[0].style.backgroundPosition="-168px 0px";
+  $(".li_3 span")[0].style.color="#00aeef";
+})
+$(".li_3").bind("mouseout",function () {
+  $(".li_3 div")[0].style.backgroundPosition="-168px -56px";
+  $(".li_3 span")[0].style.color="#fff";
+})
+
+$(".li_4").bind("mousemove",function () {
+  $(".li_4 div")[0].style.backgroundPosition="-112px -56px";
+  $(".li_4 span")[0].style.color="#00aeef";
+})
+$(".li_4").bind("mouseout",function () {
+  $(".li_4 div")[0].style.backgroundPosition="0 0";
+  $(".li_4 span")[0].style.color="#fff";
+})
+
+$(".li_5").bind("mousemove",function () {
+  $(".li_5 div")[0].style.backgroundPosition="0px -56px";
+  $(".li_5 span")[0].style.color="#00aeef";
+})
+$(".li_5").bind("mouseout",function () {
+  $(".li_5 div")[0].style.backgroundPosition="-112px 0";
+  $(".li_5 span")[0].style.color="#fff";
+})
